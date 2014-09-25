6d63e444f41d5d3cc19aa5531a99134127a772ba
Justin Rohr <justinr48@gmail.com>
2014-09-25 14:43:46 -0400

    CIS 263 HW2
    Polynomial class

---
+++ HW2/src/gvcatch.h
@@ -0,0 +1,12 @@
+//  gvcatch.h
+
+/*
+ catch.hpp is a HUGE file (47600+ lines after all the internal #includes are resolved)!
+ In order to gain the benefit by precompiled headers by some newer compilers
+ we keep two following two lines of preprocessor directives inside a separate file.
+ When your compiler supports precompile headers, you may see a file gvcatch.pch
+ in your project.
+*/
+
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"

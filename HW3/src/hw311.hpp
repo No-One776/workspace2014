1a3122fa09fe54dd47adcd907a94491629cbab0a
Justin Rohr <justinr48@gmail.com>
2014-10-08 17:03:14 -0400

    HW3 Work
    Finished all but reverse print and delete?

--- HW3/src/hw311.hpp
+++ HW3/src/hw311.hpp
@@ -6,7 +6,6 @@
 #include "gvsu_alloc.hpp"
 
 namespace gvsu {
-/* don't write any code above this line */
 using namespace std;
 
 template<class E>
@@ -35,11 +34,8 @@ private:
 	 * the head pointer below in your attempt make the design more efficient.
 	 */
 	Node *head; /* pointer to the first element */
-
 	/* You may add PRIVATE HELPER FUNCTIONS */
-
 public:
-
 	LinkedList();
 	~LinkedList();
 	int size() const;  // part (a) return the size of the linked list
@@ -47,7 +43,6 @@ public:
 	bool contains(const E& value) const; // part (c) test if the item is in the list
 	bool add(const E& value); // part (d)
 	bool remove(const E& value); // part (e) remove a value
-
 	void print_reverse(ostream&);  // prob 3.29
 };
 
@@ -58,6 +53,17 @@ LinkedList<E>::LinkedList() {
 
 template<class E>
 LinkedList<E>::~LinkedList() {
+	if (head != nullptr) {
+		Node* prev = new Node();
+		Node* next = head;
+		while (next != nullptr) {
+			prev = next;
+			next = next->next;
+			delete prev;
+		}
+		delete next;
+	} else
+		delete head;
 	/* TODO: complete this function */
 }
 
@@ -66,8 +72,16 @@ LinkedList<E>::~LinkedList() {
  */
 template<class E>
 int LinkedList<E>::size() const {
-	/* TODO: complete this function */
-	return 0;
+	int sum = 0;
+	if (head != nullptr) {
+		Node* next = new Node();
+		next = head;
+		while (next != nullptr) {
+			sum++;
+			next = next->next;
+		}
+	}
+	return sum;
 }
 
 /*
@@ -75,7 +89,15 @@ int LinkedList<E>::size() const {
  */
 template<class E>
 void LinkedList<E>::print(ostream& output) const {
-	/* TODO: complete this function */
+	if (head != nullptr) {
+		Node* next = new Node();
+		next = head;
+		while (next != nullptr) {
+			output << next->data << endl;
+			next = next->next;
+		}
+	}
+	/* TODO: Test this function */
 }
 
 /*
@@ -83,7 +105,15 @@ void LinkedList<E>::print(ostream& output) const {
  */
 template<class E>
 bool LinkedList<E>::contains(const E &value) const {
-	/* TODO: complete this function */
+	if (head != nullptr) {
+		Node* next = new Node();
+		next = head;
+		while (next != nullptr) {
+			if (next->data == value)
+				return true;
+			next = next->next;
+		}
+	}
 	return false;
 }
 
@@ -91,8 +121,24 @@ bool LinkedList<E>::contains(const E &value) const {
  in the list). Return true if the item is inserted, false otherwise */
 template<class E>
 bool LinkedList<E>::add(const E& value) {
-	/* TODO: complete this function */
-	return false;
+	if (!contains(value)) {
+		Node* insert = new Node();
+		insert->data = value;
+		insert->next = nullptr;
+		Node* next = new Node();
+		Node* prev = new Node();
+		next = head;
+		while (next != nullptr) {
+			prev = next;
+			next = next->next;
+		}
+		if (head != nullptr)
+			prev->next = insert;
+		else
+			head = insert;
+		return true;
+	} else
+		return false;
 }
 
 /* 3.11 (d) insert new_data at the end of the list (if it is not already
@@ -100,6 +146,27 @@ bool LinkedList<E>::add(const E& value) {
 template<class E>
 bool LinkedList<E>::remove(const E& value) {
 	/* TODO: complete this function */
+	if (head != nullptr) {
+		Node* next = new Node();
+		Node* prev = new Node();
+		Node* end = new Node();
+		next = head;
+		while (next != nullptr) {
+			if (next->data == value) {
+				end = next->next;
+				if (end != nullptr)
+					prev->next = end;
+				else
+					prev->next = nullptr;
+				if (next == head)
+					head = end;
+				delete next;
+				return true;
+			}
+			prev = next;
+			next = next->next;
+		}
+	}
 	return false;
 }
 
@@ -108,16 +175,17 @@ bool LinkedList<E>::remove(const E& value) {
  */
 template<class E>
 ostream& operator<<(ostream& os, const LinkedList<E>& theList) {
-	/* TODO: complete this function. Hint: just need one line of code */
+	theList.print(os);
 	return os;
 }
 
-/* Prob 3.29: print the list in reverse, using only O(1) extra space */
+/* Prob 3.29: print the list in reverse, using only O(1) extra memory space */
 template<class E>
 void LinkedList<E>::print_reverse(ostream &os) {
 	/* TODO: complete this function */
-	//Make a recursive helper function?
+
+//Make a recursive helper function?
 }
 
-} /* end of namespace, don't write any code below this line */
+}
 #endif

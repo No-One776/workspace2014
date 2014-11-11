95bfd1c891610f5abafe834bd866b26af54c2ff4
Justin Rohr <justinr48@gmail.com>
2014-11-11 14:54:08 -0500

    HW5
    developed hashOf and made changes to findFirst, but find First still
    needs to be modified to work properly. See todo comment

--- StringMatch/src/stringmatch.cpp
+++ StringMatch/src/stringmatch.cpp
@@ -11,21 +11,23 @@ using namespace std;
 
 const int PRIME_BASE = 53;  // 37 in the textbook
 long hashOf(const string& s) {
-	long hash;
-	/* TODO: calculate the hash value of the given input string.
-	 * Use a polynomial with a prime number (Refer to Section 5.2,
+	long hash = 0;
+	/* Use a polynomial with a prime number (Refer to Section 5.2,
 	 * Figure 5.4)
 	 */
+	for (char c : s)
+		hash = 37 * hash + c;
 	return hash;
 }
 
 int findFirst(const string& pattern, const string& input) {
 	/* TODO: Complete this function as described in Question 5.15 */
 	long hash1 = hashOf(pattern);
+	long hash2 = hashOf(input);
 	int length = pattern.length();
 	int inputEnd = input.length();
 	for (int x = 0; x < inputEnd - length; x++) {
-		if (hash1 == hashOf(input.substr(x, length)))
+		if (hash1 == hash2) //TODO: Actually figure out how to implement the right compare
 			if (pattern.compare(input.substr(x, length)) == 0)
 				return x;
 	}
